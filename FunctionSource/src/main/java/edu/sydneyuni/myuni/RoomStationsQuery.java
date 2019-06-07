05d232b3e4f3638f16480fe8127e150bd353f92a
hotbrainy <brainhg95@gmail.com>
2019-06-07 12:29:32 +1000

    Added more logging to RoomStationsQuery

--- FunctionSource/src/main/java/edu/sydneyuni/myuni/RoomStationsQuery.java
+++ FunctionSource/src/main/java/edu/sydneyuni/myuni/RoomStationsQuery.java
@@ -2,17 +2,21 @@ package edu.sydneyuni.myuni;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import edu.sydneyuni.myuni.models.RoomStation;
 import edu.sydneyuni.myuni.services.RoomStationDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class RoomStationsQuery implements RequestStreamHandler {
+public class RoomStationsQuery implements RequestHandler<Object, RoomStation[]> {
 
     private final RoomStationDao dao;
+    private static final Logger logger = LogManager.getLogger(RoomStationsQuery.class);
 
     @SuppressWarnings("unused") // Lambda needs a public no-arg constructor
     public RoomStationsQuery() {
@@ -23,7 +27,6 @@ public class RoomStationsQuery implements RequestStreamHandler {
         this.dao = dao;
     }
 
-    @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         RoomStation[] roomStations = getDao().getLatestRoomStations();
         getDao().getWriter().writeValue(outputStream, roomStations);
@@ -31,5 +34,16 @@ public class RoomStationsQuery implements RequestStreamHandler {
 
     private RoomStationDao getDao() {
         return dao;
+    }
+
+    @Override
+    public RoomStation[] handleRequest(Object o, Context context) {
+        try {
+            RoomStation[] roomStations = getDao().getLatestRoomStations();
+            return roomStations;
+        } catch (Exception e) {
+            logger.error("Error getting RoomStations from DynamoDB", e);
+        }
+        return new RoomStation[0];
     }
 }
