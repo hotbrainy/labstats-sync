d15516c5e60bc10327f5707fbf7061221d8fa620
hotbrainy <brainhg95@gmail.com>
2019-06-11 03:25:19 +0000

    Merged in feature/MYUNI-765 (pull request #2)
    Updated LabStats sync config to final version before prod

--- FunctionSource/src/test/java/edu/sydneyuni/myuni/models/USydCampusesTest.java
+++ FunctionSource/src/test/java/edu/sydneyuni/myuni/models/USydCampusesTest.java
@@ -11,9 +11,9 @@ public class USydCampusesTest {
 
     public static USydCampuses generate() {
         USydCampuses config = new USydCampuses();
-        config.put("Camperdown", new USydCampuses.CampusBuildings());
-        config.get("Camperdown").put("Fisher Library", new USydCampuses.CampusBuildings.BuildingRooms());
-        config.get("Camperdown").get("Fisher Library").put("Level 2", new USydCampuses.CampusBuildings.BuildingRooms.RoomStationGroups(new int[]{1215, 1216}, new int[]{1220}));
+        config.put("Camperdown Campus", new USydCampuses.CampusBuildings());
+        config.get("Camperdown Campus").put("F03 - Fisher Library", new USydCampuses.CampusBuildings.BuildingRooms());
+        config.get("Camperdown Campus").get("F03 - Fisher Library").put("Level 2", new USydCampuses.CampusBuildings.BuildingRooms.RoomStationGroups(new int[]{1215, 1216}, new int[]{1220}));
         return config;
     }
 
@@ -22,10 +22,10 @@ public class USydCampusesTest {
         ObjectMapper objectMapper = new ObjectMapper();
         USydCampuses config = objectMapper.readValue(getClass().getClassLoader().getResourceAsStream("labstats.json"), USydCampuses.class);
         assertNotNull(config);
-        assertNotNull(config.get("Camperdown"));
-        assertNotNull(config.get("Camperdown").get("Fisher Library"));
-        assertNotNull(config.get("Camperdown").get("Fisher Library").get("Level 2"));
-        assertNotNull(config.get("Camperdown").get("Fisher Library").get("Level 2").getPcGroups());
-        assertNotNull(config.get("Camperdown").get("Fisher Library").get("Level 2").getPodGroups());
+        assertNotNull(config.get("Camperdown / Darlington Campus"));
+        assertNotNull(config.get("Camperdown / Darlington Campus").get("F03 - Fisher Library"));
+        assertNotNull(config.get("Camperdown / Darlington Campus").get("F03 - Fisher Library").get("Level 2"));
+        assertNotNull(config.get("Camperdown / Darlington Campus").get("F03 - Fisher Library").get("Level 2").getPcGroups());
+        assertNotNull(config.get("Camperdown / Darlington Campus").get("F03 - Fisher Library").get("Level 2").getPodGroups());
     }
 }
