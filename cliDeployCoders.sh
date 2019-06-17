d4d04d77e1fd0652308a866728548940f05ed7d6
hotbrainy <brainhg95@gmail.com>
2019-06-17 10:31:01 +1000

    Updated build to include $LABSTATS_API_KEY
    Updated .gitignore to remove build files.

--- cliDeployCoders.sh
+++ cliDeployCoders.sh
@@ -3,4 +3,5 @@ CODERS_AWS_BUCKET="cf-templates-u6grlzjqw4qg-ap-southeast-2"
 sam build -b ./build -t ./template.yaml
 sam package --template-file build/template.yaml --output-template-file packaged.yaml --s3-bucket $CODERS_AWS_BUCKET
 rm -fr ./build
-sam deploy --template-file packaged.yaml --parameter-overrides Lifecycle=Development Environment=D Application=lbst --capabilities CAPABILITY_IAM --stack-name lbstSyncFunc
+sam deploy --template-file packaged.yaml --parameter-overrides Environment=C Application=lbst LabStatsAPIKey=$LABSTATS_API_KEY --capabilities CAPABILITY_IAM --stack-name lbstSyncFunc
+rm packaged.yaml
