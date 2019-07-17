faa936e8006774186198be8d1af9a3c09fe9b63a
Andre Verheij <andre.verheij@sydney.edu.au>
2019-07-17 14:54:49 +1000

    correct definition of Webhook for BitBucket..

--- cliDeployPipelineDev.sh
+++ cliDeployPipelineDev.sh
@@ -3,6 +3,6 @@ BranchName=develop
 StackName=lbstCodeBuild${BranchName}
 aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides Application=lbst BranchName=${BranchName} --stack-name ${StackName}
 ProjectName=$(aws cloudformation describe-stacks --stack-name ${StackName} --query 'Stacks[0].Outputs[?OutputKey==`ProjectName`].OutputValue' --output text)
-aws codebuild create-webhook \
-    --project-name ${ProjectName} \
-    --filter-groups "[[{\"type\":\"EVENT\",\"pattern\":\"PUSH\"},{\"type\":\"HEAD_REF\",\"pattern\":\"^refs/heads/${BranchName}$\",\"excludeMatchedPattern\":false}]]"
+#aws codebuild create-webhook \
+#    --project-name ${ProjectName} \
+#    --filter-groups "[[{\"type\":\"EVENT\",\"pattern\":\"PUSH\"},{\"type\":\"HEAD_REF\",\"pattern\":\"^refs/heads/${BranchName}$\",\"excludeMatchedPattern\":false}]]"
