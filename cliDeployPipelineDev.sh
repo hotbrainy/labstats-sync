add5ea476257bf0a40083e941b48cb27b57d4810
Andre Verheij <andre.verheij@sydney.edu.au>
2019-07-17 22:44:33 +1000

    Fixed up stack variables.

--- cliDeployPipelineDev.sh
+++ cliDeployPipelineDev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 BranchName=develop
 StackName=lbstCodeBuild${BranchName}
-aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides Application=lbst BranchName=${BranchName} --stack-name ${StackName}
+aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides ParentVPCStack=ictdevawsvpc Application=lbst BranchName=${BranchName} --stack-name ${StackName}
 #ProjectName=$(aws cloudformation describe-stacks --stack-name ${StackName} --query 'Stacks[0].Outputs[?OutputKey==`ProjectName`].OutputValue' --output text)
 #aws codebuild create-webhook \
 #    --project-name ${ProjectName} \
--- cliDeployPipelineTest.sh
+++ cliDeployPipelineTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 BranchName=release
 StackName=lbstCodeBuild${BranchName}
-aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides Application=lbst BranchName=${BranchName} --stack-name ${StackName}
+aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides Application=lbst ParentVPCStack=icttestawsvpc BranchName=${BranchName} --stack-name ${StackName}
 #ProjectName=$(aws cloudformation describe-stacks --stack-name ${StackName} --query 'Stacks[0].Outputs[?OutputKey==`ProjectName`].OutputValue' --output text)
 #aws codebuild create-webhook \
 #    --project-name ${ProjectName} \
