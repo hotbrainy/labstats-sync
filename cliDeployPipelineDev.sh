f7d0eb150761990aade4a3dba077ea44caee0d2e
Andre Verheij <andre.verheij@sydney.edu.au>
2019-07-17 15:13:52 +1000

    Fixed up codebuild creation scripts, to test if it can handle the release branch.

--- cliDeployPipelineDev.sh
+++ cliDeployPipelineDev.sh
@@ -2,7 +2,7 @@
 BranchName=develop
 StackName=lbstCodeBuild${BranchName}
 aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides Application=lbst BranchName=${BranchName} --stack-name ${StackName}
-ProjectName=$(aws cloudformation describe-stacks --stack-name ${StackName} --query 'Stacks[0].Outputs[?OutputKey==`ProjectName`].OutputValue' --output text)
+#ProjectName=$(aws cloudformation describe-stacks --stack-name ${StackName} --query 'Stacks[0].Outputs[?OutputKey==`ProjectName`].OutputValue' --output text)
 #aws codebuild create-webhook \
 #    --project-name ${ProjectName} \
 #    --filter-groups "[[{\"type\":\"EVENT\",\"pattern\":\"PUSH\"},{\"type\":\"HEAD_REF\",\"pattern\":\"^refs/heads/${BranchName}$\",\"excludeMatchedPattern\":false}]]"
