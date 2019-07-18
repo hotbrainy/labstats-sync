6194dca7b0340b5a7e472386ff3873926c70ee86
Andre Verheij <andre.verheij@sydney.edu.au>
2019-07-18 01:12:55 +0000

    Merged in develop (pull request #7)
    Fixed up branch names input.
    
    Approved-by: Philip Bui <philip.bui@sydney.edu.au>

--- cliDeployPipelineDev.sh
+++ cliDeployPipelineDev.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 BranchName=develop
 StackName=lbstCodeBuild${BranchName}
-aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides ParentVPCStack=ictdevawsvpc Application=lbst BranchName=${BranchName} --stack-name ${StackName}
+Application=lbst
+ParentVPCStack=ictdevawsvpc
+aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides Application=${Application} ParentVPCStack=${ParentVPCStack} BranchName=${BranchName} --stack-name ${StackName}
 #ProjectName=$(aws cloudformation describe-stacks --stack-name ${StackName} --query 'Stacks[0].Outputs[?OutputKey==`ProjectName`].OutputValue' --output text)
 #aws codebuild create-webhook \
 #    --project-name ${ProjectName} \
