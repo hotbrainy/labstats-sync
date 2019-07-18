bb55687f0d4ee4d69c92619114950ca39dfa8c01
Andre Verheij <andre.verheij@sydney.edu.au>
2019-07-18 18:56:40 +1000

    Added environment variable for environment to passed through.

--- cliDeployPipelineTest.sh
+++ cliDeployPipelineTest.sh
@@ -3,7 +3,8 @@ BranchName=test
 StackName=lbstCodeBuild${BranchName}
 Application=lbst
 ParentVPCStack=icttestawsvpc
-aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides Application=${Application} ParentVPCStack=${ParentVPCStack} BranchName=${BranchName} --stack-name ${StackName}
+Environment=P
+aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides Environment=${Environment} Application=${Application} ParentVPCStack=${ParentVPCStack} BranchName=${BranchName} --stack-name ${StackName}
 #ProjectName=$(aws cloudformation describe-stacks --stack-name ${StackName} --query 'Stacks[0].Outputs[?OutputKey==`ProjectName`].OutputValue' --output text)
 #aws codebuild create-webhook \
 #    --project-name ${ProjectName} \
