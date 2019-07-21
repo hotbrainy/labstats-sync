6091974c9e1513d32e88ea71a6562326aa50718a
Andre Verheij <andre.verheij@sydney.edu.au>
2019-07-22 09:54:36 +1000

    Fixed up error in deployment script.

--- cliDeployPipelineTest.sh
+++ cliDeployPipelineTest.sh
@@ -3,7 +3,7 @@ BranchName=test
 StackName=lbstCodeBuild${BranchName}
 Application=lbst
 ParentVPCStack=icttestawsvpc
-Environment=P
+Environment=T
 aws cloudformation deploy --template-file lbstInfra.yaml --capabilities CAPABILITY_IAM --parameter-overrides Environment=${Environment} Application=${Application} ParentVPCStack=${ParentVPCStack} BranchName=${BranchName} --stack-name ${StackName}
 #ProjectName=$(aws cloudformation describe-stacks --stack-name ${StackName} --query 'Stacks[0].Outputs[?OutputKey==`ProjectName`].OutputValue' --output text)
 #aws codebuild create-webhook \
